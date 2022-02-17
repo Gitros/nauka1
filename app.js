d4dc83c7cbff534af291796b7b873c55f2238f95
Gitros <31899790+Gitros@users.noreply.github.com>
2022-02-17 19:18:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const name = 'john';
-const lastName = 'shakeandbake';
-let fullName;
-console.log('hello there your fullname is : ' + name + ' ' + lastName);
+//const name = 'john';
+//const lastName = 'shakeandbake';
+//let fullName;
+//console.log('hello there your fullname is : ' + name + ' ' + lastName);
+
+const website = 'google';
+const url = 'https:/www.' + website + '.com';
+console.log(url);
