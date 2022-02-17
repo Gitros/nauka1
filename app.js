45dda14a98088a8bd1e114dbcca4fc68e5224202
Gitros <31899790+Gitros@users.noreply.github.com>
2022-02-17 19:19:02 +0100

    js clear

--- app.js
+++
@@ -1,8 +0,0 @@
-//const name = 'john';
-//const lastName = 'shakeandbake';
-//let fullName;
-//console.log('hello there your fullname is : ' + name + ' ' + lastName);
-
-const website = 'google';
-const url = 'https:/www.' + website + '.com';
-console.log(url);
