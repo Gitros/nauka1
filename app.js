9a9171d843efc2db4f03d38a34086448535fa010
Gitros <31899790+Gitros@users.noreply.github.com>
2022-02-19 00:12:33 +0100

    data types

---
+++ app.js
@@ -0,0 +1,36 @@
+//data types
+//primitive - string number boolean null undefined symbol
+// object - arrays functions objects
+
+// typeof - operator (typeof variable) (tybeof value)
+
+//string
+
+const text = 'some text';
+
+//number
+
+const number = 45;
+
+//boolean
+
+let value1 = true;
+let value2 = false;
+
+//null
+
+const result = null;
+
+//undefined
+
+let name;
+//symbol(es6)
+
+console.log(typeof text);
+console.log(typeof 3);
+console.log(typeof true);
+console.log(typeof number);
+console.log(typeof value1);
+console.log(typeof value2);
+console.log(typeof null);
+console.log(typeof name);
