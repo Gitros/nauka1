e65e19e708af16c98bd8ef1ee73a29ae74423959
Gitros <31899790+Gitros@users.noreply.github.com>
2022-02-18 23:43:39 +0100

    math

---
+++ app.js
@@ -0,0 +1,22 @@
+let number = 40;
+number += 5;
+number += 5;
+number += 5;
+number += 5;
+number -= 5;
+number++;
+number--;
+number++;
+number++;
+
+const slices = 10;
+const children = 4;
+const amount = slices % children;
+
+const random = 4 + 6 + 10 * 10;
+const random2 = (4 + 6 + 10) * 10;
+
+console.log(number);
+console.log(amount);
+console.log(random);
+console.log(random2);
