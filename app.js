092c69a370b96af1f59fae2c5f8d3e77d8ee1523
Gitros <31899790+Gitros@users.noreply.github.com>
2022-02-23 18:24:08 +0100

    arrays

---
+++ app.js
@@ -0,0 +1,14 @@
+const friend1 = 'anna';
+const friend2 = 'anna';
+const friend3 = 'anna';
+const friend4 = 'anna';
+
+const friends = ['john', 'peter', 'bob', 'susy', 45, undefined, null];
+
+let bestFriend = friends[2];
+friends[4] = 'anna';
+
+console.log(friends);
+console.log(friends[4]);
+console.log(bestFriend);
+console.log(friends[3]);
