56c7a8b5fdc8c7ac3f42cb84b955dfe003f474a1
Gitros <31899790+Gitros@users.noreply.github.com>
2022-02-23 19:56:14 +0100

    arrays

---
+++ app.js
@@ -0,0 +1,16 @@
+function hello() {
+  //logic
+}
+
+console.log('hello there bob');
+console.log('hello there anna');
+console.log('hello there susy');
+// code here
+console.log('hello there bob');
+console.log('hello there anna');
+console.log('hello there susy');
+//code here
+console.log('hello there bob');
+console.log('hello there anna');
+console.log('hello there susy');
+//code here
