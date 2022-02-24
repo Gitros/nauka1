1952c60bdcff4b4849d47e7e865dff96351103a4
Gitros <31899790+Gitros@users.noreply.github.com>
2022-02-24 23:36:38 +0100

    function - expressions

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+//function declaration
 function addValues(num1, num2) {
   return num1 + num2;
 }
 
 const firstValue = addValues(3, 4);
 const secondValue = addValues(12, 34);
-
-const values = [firstValue, secondValue];
+//function expression
+const add = function (num1, num2) {
+  return num1 + num2;
+};
+const thirdValue = add(5, 6);
+const values = [firstValue, secondValue, thirdValue, add(6, 7)];
 console.log(values);
