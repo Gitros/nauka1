dafed75428506923f412a0c36c487454b3e4bb5a
Gitros <31899790+Gitros@users.noreply.github.com>
2022-02-24 23:55:46 +0100

    statements pt3

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const value = true;
-
+const value = 2 > 1;
+console.log(typeof value);
+const value2 = 1 > 2;
 if (value) {
   console.log('hello world');
+} else {
+  console.log('hello people');
 }
