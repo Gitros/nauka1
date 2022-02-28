37e22527987136f64d94f3bf482d7b822e52fd51
Gitros <31899790+Gitros@users.noreply.github.com>
2022-02-28 23:17:35 +0100

    statement pt2

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const dice = 1;
-if (dice === 1) {
-  console.log('you got one');
-}
-if (dice === 2) {
-  console.log('you got two');
-}
-if ((dice < 1) | (dice > 6)) {
-  console.log('you did not roll the dice');
+
+switch (dice) {
+  case 1:
+    console.log('you got one');
+    break;
+  case 2:
+    console.log('you got two');
+    break;
+  case 3:
+    console.log('you got three');
+    break;
+  default:
+    console.log('you did not roll the dice');
 }
