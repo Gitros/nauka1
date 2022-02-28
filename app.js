b04fe7d6418723f7c9e8fd8ca4e45d49e29543fa
Gitros <31899790+Gitros@users.noreply.github.com>
2022-02-28 23:09:30 +0100

    switch statement

---
+++ app.js
@@ -0,0 +1,10 @@
+const dice = 1;
+if (dice === 1) {
+  console.log('you got one');
+}
+if (dice === 2) {
+  console.log('you got two');
+}
+if ((dice < 1) | (dice > 6)) {
+  console.log('you did not roll the dice');
+}
