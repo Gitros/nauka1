8a13f6ea675f749fd07803dd958d9eea4d6ca60d
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-03 21:24:11 +0100

    template literals pt2.

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ const name = 'john';
 const age = 25;
 const sentence = "Hey it's " + name + ' and he is ' + age + ' years old';
 
+const value = `Hey it's ${name} and he is ${age} years old. And here is some simple math ${
+  4 + 4
+}`;
+
+console.log(value);
+
 console.log(sentence);
