deb593b2837be4209ddf3c965a1be33e371866fe
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-03 22:19:01 +0100

    arrays and for loop

---
+++ app.js
@@ -0,0 +1,16 @@
+//arrays and for loop
+
+const names = ['anna', 'susy', 'bob', 'john', 'susan'];
+const lastName = 'shakeandbake';
+let newArray = [];
+
+//for loop
+
+for (let i = 0; i < names.length; i++) {
+  console.log(i);
+  console.log(names[i]);
+  newArray.push(`${names[i]} ${lastName}`);
+}
+
+console.log(names);
+console.log(newArray);
