1a3f401bb9b9c9523988fac48fc326b09801c4ac
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-04 23:13:29 +0100

    ternary operator

---
+++ app.js
@@ -0,0 +1,8 @@
+//unary operator - typeof
+let text = 'some text';
+console.log(typeof text); // operand
+//binary operator - assignment
+let number = 3;
+let number2 = 2 + 5;
+//ternary operator
+//condition ? (runs if true) : (runs if false)
