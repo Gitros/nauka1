80a6de414a9ac0f79efd9b075b69a9386f1c7b1c
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-04 23:14:42 +0100

    value is true

--- app.js
+++ app.js
@@ -6,3 +6,10 @@ let number = 3;
 let number2 = 2 + 5;
 //ternary operator
 //condition ? (runs if true) : (runs if false)
+
+const value = 2 > 1;
+if (value) {
+  console.log('value is true');
+} else {
+  console.log('value is false');
+}
