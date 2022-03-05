e222cda7f6f2e469c3a7735a032e4f783a16ca4d
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-06 00:50:35 +0100

    variable lookup

---
+++ app.js
@@ -0,0 +1,11 @@
+//variable lookup
+// {} - code block
+
+const globalNumber = 5;
+
+function add(num1, num2) {
+  const result = num1 + num2 + globalNumber;
+  return result;
+}
+
+console.log(add(3, 4));
