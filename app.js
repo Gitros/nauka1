b7d070e70fddcc522ef38ec5719fd08be4e48e51
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-06 00:47:57 +0100

    update

--- app.js
+++ app.js
@@ -9,11 +9,18 @@ function calculate() {
   const name = 'john';
   const age = 25;
   //code goes here
+  becomesGlobal = 'global variable';
 }
-console.log(age);
-calculate();
+console.log(becomesGlobal);
+// calculate();
 
 if (true) {
   const name = 'john';
 }
+
+{
+  const name = 'john';
+  const special = 'special';
+}
+console.log(special);
 console.log(`my name is ${name} and I'm awesome`);
