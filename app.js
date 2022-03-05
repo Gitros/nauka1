60887173ba655d3d49a4883fc906da499249e000
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-05 23:56:39 +0100

    global scope

---
+++ app.js
@@ -0,0 +1,7 @@
+//global scope vs local scope
+//any variable outside code block {} is said to be in global scope
+//can be access anywhere int the program
+//Gotchas : name collisions, modify by mistake
+
+let name = 'bobo';
+name = 'peter';
