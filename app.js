b6fdd6d6a3d3556bb6a9849f5fb4006066f549d7
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-06 00:05:46 +0100

    clear for local scope

--- app.js
+++ app.js
@@ -4,21 +4,10 @@
 //Gotchas : name collisions, modify by mistake
 
 let name = 'bobo';
-name = 'peter';
 
-function calculate() {
-  console.log(name);
-  name = 'orange';
-  function inner() {
-    name = 'inner name value';
-    console.log(`this is from inner function ${name}`);
-  }
-  inner();
-}
+function calculate() {}
 calculate();
 
 if (true) {
-  console.log(name);
-  name = 'pants';
 }
 console.log(`my name is ${name} and I'm awesome`);
