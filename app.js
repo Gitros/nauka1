f8c833cf1ceeb0c105eb27baf3161ef69500a700
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-07 21:16:04 +0100

    callback functions higher order

---
+++ app.js
@@ -0,0 +1,14 @@
+function greetMorning(name) {
+  const myName = 'john';
+  console.log(`Good morning ${name}, my name is ${myName}`);
+}
+
+greetMorning('bobo');
+
+//callback functions higher order functions functions as first class objects/ citizens
+
+//function are first class objects - stored in a variable passed as an argument to another function return form the function
+
+//higher order function - accepts another function as an argument or returns another function as a result
+
+// callback function - passed to a another function as an an argomuent and executed inside that function
