63c120cd99134f54ebdceb6976474a0e454d602d
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-08 20:16:06 +0100

    clear

--- app.js
+++ app.js
@@ -9,17 +9,3 @@ const people = [
   { name: 'susy', age: 30, position: 'the boss' },
   { name: 'anna', age: 35, position: 'the boss' },
 ];
-
-const ages = people.map(function (person) {
-  return person.age + 20;
-});
-const newPeople = people.map(function (person) {
-  return { firstName: person.name.toUpperCase(), oldAge: person.age + 20 };
-});
-
-const names = people.map(function (person) {
-  return `<h1>${person.name}</h1>`;
-});
-
-document.body.innerHTML = names.join('');
-console.log(names);
