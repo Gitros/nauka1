09d9d73ed970ee1368112f28dd89daf2da96c0bf
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-08 20:37:21 +0100

    clear

--- app.js
+++ app.js
@@ -9,20 +9,3 @@ const people = [
   { name: 'susy', age: 30, position: 'the boss', id: 3 },
   { name: 'anna', age: 35, position: 'the boss', id: 4 },
 ];
-const names = ['bob', 'peter', 'susy'];
-console.log(
-  names.find(function (name) {
-    return name === 'zelda';
-  })
-);
-const person = people.find(function (person) {
-  return person.id === 3;
-});
-
-console.log(person.name);
-
-const person2 = people.filter(function (person) {
-  return person.id === 3;
-});
-
-console.log(person2[0].name);
