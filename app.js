dbaec25c4810edb89cbf6554b8abd5a671deb607
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-08 20:48:48 +0100

    reduce

--- app.js
+++ app.js
@@ -10,3 +10,12 @@ const people = [
   { name: 'susy', age: 30, position: 'the boss', id: 3, salary: 500 },
   { name: 'anna', age: 35, position: 'the boss', id: 4, salary: 500 },
 ];
+
+const total = people.reduce(function (acc, currItem) {
+  console.log(`total ${acc}`);
+  console.log(`current money : ${currItem.salary}`);
+  acc += currItem.salary;
+  return acc;
+}, 500);
+
+console.log(total);
