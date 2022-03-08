b9bb27f1c94a3c8f6c5d5c34be2f9f1bf3e6bb82
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-08 18:43:03 +0100

    for each

---
+++ app.js
@@ -0,0 +1,9 @@
+//forEach
+//does not return new array
+
+
+const people = [
+ {name: 'bob', age:20,position:'developer'}
+ {name: 'peter', age:25,position:'designer'}
+ {name: 'susy', age:30,position:'the boss'}
+]
