5cadbd9b2a2d280cdde200beefab34984473c9ff
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-09 22:57:24 +0100

    square root

--- app.js
+++ app.js
@@ -4,7 +4,10 @@
 // const number = 4.56789;
 // const result = Math.floor(number);
 
-const number = 4.56789;
-const result = Math.ceil(number);
+// const number = 4.12222;
+// const result = Math.ceil(number);
+
+const number = 89;
+const result = Math.sqrt(number);
 
 console.log(result);
