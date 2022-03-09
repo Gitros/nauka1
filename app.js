5b0f29fdc71250300b1f79e83d6d350318897086
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-09 22:55:53 +0100

    math round up

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 //math
 // standard built-in object - always avaible
 
+// const number = 4.56789;
+// const result = Math.floor(number);
+
 const number = 4.56789;
-const result = Math.floor(number);
+const result = Math.ceil(number);
 
 console.log(result);
