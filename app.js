90eeaf25b4687f15b58e4102178a34ae62245af4
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-09 23:11:45 +0100

    date

---
+++ app.js
@@ -0,0 +1,30 @@
+// Date
+
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturtday',
+];
+
+const date = new Date();
+const month = date.getMonth();
+console.log(months[month]);
