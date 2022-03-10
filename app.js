6a0a40660a60454fb961f86c0f72bb42c19b9d17
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-10 23:36:21 +0100

    DOM

---
+++ app.js
@@ -0,0 +1,16 @@
+// similiar to css
+
+// select the element or group of elements the you we want to affect
+// decide the effect we want to apply to the selection
+
+//many different was
+
+//document.body.style.bacgkroundColor='blue;
+// document.body.style.color = 'yellow';
+// document.getElementById('btn').style.color = 'red';
+
+//assign to a variable
+const element = document.getElementById('element');
+//do something
+
+document.querySelector('element'); // do something
