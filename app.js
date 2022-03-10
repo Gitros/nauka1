64260b3c67848c86ae277492f06b5c34a8478f8c
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-10 23:46:44 +0100

    pt4

--- app.js
+++ app.js
@@ -14,3 +14,16 @@ const element = document.getElementById('element');
 //do something
 
 document.querySelector('element'); // do something
+
+//window object
+console.log(window);
+
+//returns a node object or a node list, which is an arraylike object
+
+const btn = document.getElementById('btn');
+const name = btn.nodeName;
+const css = btn.style;
+
+console.log(btn);
+console.log(name);
+console.log(css);
