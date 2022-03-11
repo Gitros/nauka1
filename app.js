29f6fd8228f0de54c79850f4b3dd65a66cf922d6
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-11 23:59:41 +0100

    getelementbyid

---
+++ app.js
@@ -0,0 +1,10 @@
+// Select the element or group of elements the we want
+// Decide the effect we want to apply to the selection
+
+// getElementById('element')
+
+const h1 = document.getElementById('title');
+h1.style.color = 'red';
+
+document.getElementById('btn').style.backgroundColor = 'blue';
+document.getElementById('btn').style.color = 'white';
