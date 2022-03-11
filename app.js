ad85b3cefbe81cd480f8d1e45b23fcc077afed5f
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-12 00:00:06 +0100

    dsa

--- app.js
+++ app.js
@@ -6,5 +6,4 @@
 const h1 = document.getElementById('title');
 h1.style.color = 'red';
 
-document.getElementById('btn').style.backgroundColor = 'blue';
-document.getElementById('btn').style.color = 'white';
+const btn = document.getElementById('btn');
