6d8894f085116ed78de7b9e1bc66241298a89b16
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-12 13:24:35 +0100

    pt3

--- app.js
+++ app.js
@@ -7,3 +7,14 @@
 
 const headings = document.getElementsByTagName('h2');
 headings[0].style.color = 'red';
+// console.log(headings.length);
+
+const items = document.getElementsByTagName('li');
+// items.forEach(function (item) {
+//   console.log(item);
+// });
+items[2].style.color = 'orange';
+const betterItems = [...items];
+betterItems.forEach(function (item) {
+  console.log(item);
+});
