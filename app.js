3f2b26e2a8f9de626f92480817f617c85f118825
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-12 13:18:29 +0100

    pt2

--- app.js
+++ app.js
@@ -4,3 +4,6 @@
 //getElementsByTagName('tagname');
 //node-list = array-like object
 // index, length property but not array methods
+
+const headings = document.getElementsByTagName('h2');
+headings[0].style.color = 'red';
