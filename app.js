674300b48338bfc79f97803be27298e5c8fbce0a
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-12 13:16:08 +0100

    get elements by tag names

---
+++ app.js
@@ -0,0 +1,6 @@
+//Select the element or group of elements the we want
+//Decide the effect we want to apply to the selection
+
+//getElementsByTagName('tagname');
+//node-list = array-like object
+// index, length property but not array methods
