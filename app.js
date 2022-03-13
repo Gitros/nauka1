96a6ee8bd13e4d6e01d92f25282512668153b448
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-13 23:11:53 +0100

    pt 2

--- app.js
+++ app.js
@@ -6,3 +6,10 @@
 // children
 // firstChild
 // lastChild
+
+const result = document.querySelector('#result');
+const allChildren = result.childNodes;
+// console.log(allChildren);
+
+const children = result.children;
+console.log(children);
