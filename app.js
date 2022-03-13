9e60b635921bc4880773c7572669acde3e84b956
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-13 23:29:56 +0100

    clear

--- app.js
+++
@@ -1,7 +0,0 @@
-// previousElementSibling
-//nextElementSibling
-
-const first = document.querySelector('.first');
-first.nextElementSibling.style.color = 'red';
-
-const last = document.querySelector('#last');
