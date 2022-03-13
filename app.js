1e4f9e55db3446e506bea5d712d308953f6e31c3
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-13 23:06:20 +0100

    querySelectorAll

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ const result = document.querySelector('#result');
 result.style.backgroundColor = 'blue';
 
 const item = document.querySelector('.special');
-console.log(item);
+// console.log(item);
 
 const lastItem = document.querySelector('li:last-child');
-console.log(lastItem);
+// console.log(lastItem);
+
+const list = document.querySelectorAll('.special');
+console.log(list);
+
+list.forEach(function (item) {
+  console.log(item);
+  item.style.color = 'yellow';
+});
