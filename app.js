472e8960eac086906a02a73b198c2a0376cf222d
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-13 22:54:19 +0100

    get elements by class name

---
+++ app.js
@@ -0,0 +1,7 @@
+// Select the element or group of elements the we want
+// Decide the effect we want to apply to selection
+
+// getElementsByClassName('classname');
+
+//node-list = array-like object
+//index, lenght property but not array methods
