01a36cb1d01ad1e0d7603727ff17537fe520360b
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-13 23:08:04 +0100

    navigate the DOM

---
+++ app.js
@@ -0,0 +1,8 @@
+// Select the element or group of elements the we want
+// Decide the effect we want to apply to the selection
+
+// childNodes - returns all childNodes including whitespace which is treated as a text node
+
+// children
+// firstChild
+// lastChild
