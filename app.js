f433a7c386639da43918a6283c17b9f6188a7e83
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-13 23:02:00 +0100

    querySelector and QueryselectorAll

---
+++ app.js
@@ -0,0 +1,8 @@
+// Select the element or group of elements the we want
+// Decide the effect we want to apply to the selection
+
+// querySelector('any css'); - selects single
+// querySelectorAll('any css') - selects all
+
+const result = document.querySelector('#result');
+result.style.backgroundColor = 'blue';
