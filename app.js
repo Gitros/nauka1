da17af9e1eca2e002cadfeaf49eee42cae425a7a
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-14 20:34:23 +0100

    textContent and NodeValue

---
+++ app.js
@@ -0,0 +1,8 @@
+//node value
+//textContent
+
+const item = document.getElementById('special');
+const value = item.firstChild.nodeValue;
+const easyValue = item.textContent;
+
+console.log(easyValue);
