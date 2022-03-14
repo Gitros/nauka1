51bfb3246c51dd37cb226b7fa52502118d5c2e02
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-14 20:45:55 +0100

    setAtribute

--- app.js
+++ app.js
@@ -3,7 +3,16 @@
 
 const first = document.querySelector('.first');
 const idValue = first.getAttribute('id');
-console.log(idValue);
+// console.log(idValue);
+
+const link = document.getElementById('link');
+const showLink = link.getAttribute('href');
+console.log(showLink);
+
+const last = link.nextElementSibling;
+last.setAttribute('class', 'first');
+last.textContent = 'i also have a class of first';
+console.log(last);
 
 const links = document.querySelectorAll('.first');
 console.log(links);
