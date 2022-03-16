f0a1348cb4795ea203c54ae8adb6efd6b33dc96b
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-16 17:34:39 +0100

    create element

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 // createElement('element')
 // createTextNode('text content')
 // element.appendChildElement(childElement)
+
+const result = document.querySelector('#result');
+
+// create empty element
+
+const bodyDiv = document.createElement('div');
+// create text node
+const text = document.createTextNode('a simple body div');
