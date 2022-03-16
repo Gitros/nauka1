c391ddff4e7e416ec802bc78d02887900a70bda0
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-16 18:23:02 +0100

    better way

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 
 const random = document.querySelector('.random');
 
-random.style.backgroundColor = 'blue';
-random.style.color = 'white';
-random.style.fontSize = '3rem';
-random.style.textTransform = 'capitalize';
+// random.style.backgroundColor = 'blue';
+// random.style.color = 'white';
+// random.style.fontSize = '3rem';
+// random.style.textTransform = 'capitalize';
+
+random.classList.add('title');
