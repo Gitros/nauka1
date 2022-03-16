5fcedd5d080f7bf26a8a196472059b5b73d78cb0
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-16 18:19:21 +0100

    clear, change css with style property start

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
-// innerHTML
-// textContent
-
-const list = document.getElementById('first');
-const div = document.getElementById('second');
-const item = document.querySelector('.item');
-
-console.log(div.textContent);
-console.log(list.innerHTML);
-console.log(list.textContent);
-
-const randomVar = 'random value';
-
-const ul = document.createElement('ul');
-ul.innerHTML = `<li class="item">${randomVar}</li>
-    <li>list item</li>`;
-document.body.appendChild(ul);
+//css
