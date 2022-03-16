029845aeb61410b437774e29b476805b04c554b8
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-16 18:22:23 +0100

    css change

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 //css
+
+const random = document.querySelector('.random');
+
+random.style.backgroundColor = 'blue';
+random.style.color = 'white';
+random.style.fontSize = '3rem';
+random.style.textTransform = 'capitalize';
