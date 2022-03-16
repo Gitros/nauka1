459a4c4f8988138c796b19fd0016a12d1dc377ed
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-16 17:41:18 +0100

    create element

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ const result = document.querySelector('#result');
 const bodyDiv = document.createElement('div');
 // create text node
 const text = document.createTextNode('a simple body div');
+bodyDiv.appendChild(text);
+document.body.appendChild(bodyDiv);
+
+const heading = document.createElement('h2');
+const headingText = document.createTextNode('dynamic heading');
+heading.appendChild(headingText);
+heading.classList.add('blue');
+result.appendChild(heading);
+
+console.log(result.children);
