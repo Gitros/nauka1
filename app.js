ac50ecb1f81feed643d68fade5926e1eac160d30
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-16 18:08:57 +0100

    innerHTML and textContent

--- app.js
+++
@@ -1,8 +0,0 @@
-// remove
-// remove child
-
-const result = document.querySelector('#result');
-// result.remove();
-const heading = document.querySelector('h1');
-result.removeChild(heading);
-console.log(heading);
