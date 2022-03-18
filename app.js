220361fb816fb5e58bb3e4dc60b9b249f0a5689d
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-18 23:30:36 +0100

    pt2

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 // select element
 // addEventListener()
 // what event, what to do
+
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', function () {
+  console.log('hey you clicked me');
+});
