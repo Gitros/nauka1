aba2ca844800a230472c6e770d8035dafaf8bde4
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-18 23:32:28 +0100

    class add event

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 // what event, what to do
 
 const btn = document.querySelector('.btn');
+const heading = document.querySelector('h2');
 
 btn.addEventListener('click', function () {
-  console.log('hey you clicked me');
+  heading.classList.add('red');
 });
