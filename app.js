58afe370b3ab323674f191309306fd1fc1a431b8
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-19 23:56:14 +0100

    mouse events

---
+++ app.js
@@ -0,0 +1,8 @@
+//click - fires after full action occurs
+//mousedown-button is pressed
+// mousedown-button is released
+// mouseenter - moved onto an element
+//mouseleave - moved out of an element
+
+const heading = document.querySelector('h1');
+const btn = document.querySelector('.btn');
