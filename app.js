a8e98d31c83ba4fc228a89fb267bc1a9468ba799
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-19 23:57:42 +0100

    pt2

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 //click - fires after full action occurs
 //mousedown-button is pressed
-// mousedown-button is released
+// mouseup-button is released
 // mouseenter - moved onto an element
 //mouseleave - moved out of an element
 
 const heading = document.querySelector('h1');
 const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', function () {
+  console.log('you clicked me');
+});
+btn.addEventListener('mousedown', function () {
+  console.log('down');
+});
+btn.addEventListener('mouseup', function () {
+  console.log('up');
+});
