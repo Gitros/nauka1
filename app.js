31bf06b40a134755f21d3de0e4c12289855615c9
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-19 23:54:05 +0100

    function reference pt2

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const btn = document.querySelector('.btn');
 const heading = document.querySelector('h2');
 
 function changeColors() {
-  console.log('hello');
+  let hasClass = heading.classList.contains('red');
+  if (hasClass) {
+    heading.classList.remove('red');
+  } else {
+    heading.classList.add('red');
+  }
 }
 
 btn.addEventListener('click', changeColors);
