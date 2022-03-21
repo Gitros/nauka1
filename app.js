467f4d14766cb534a2199255bfb06ab6252758ff
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-21 21:58:50 +0100

    target

--- app.js
+++ app.js
@@ -4,7 +4,9 @@
 const btns = document.querySelectorAll('.btn');
 btns.forEach(function (btn) {
   btn.addEventListener('click', function (e) {
-    console.log(e.currentTarget);
-    e.currentTarget.style.color = 'green';
+    // console.log(e.currentTarget);
+    // e.currentTarget.style.color = 'green';
+    console.log('target', e.target);
+    e.target.style.color = 'green';
   });
 });
