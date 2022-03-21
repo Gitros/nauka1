bbea49bf96e0cabd186df8af7fa5ecb798a47690
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-21 21:33:28 +0100

    keydown

--- app.js
+++ app.js
@@ -4,6 +4,10 @@
 
 const nameInput = document.getElementById('name');
 
-nameInput.addEventListener('keypress', function () {
+// nameInput.addEventListener('keypress', function () {
+//   console.log('you pressed a key');
+// });
+
+nameInput.addEventListener('keydown', function () {
   console.log('you pressed a key');
 });
