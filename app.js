e9a079de5d1e4cf21962b361d7c713f859c7438c
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-21 21:56:59 +0100

    currentTarget vs target

---
+++ app.js
@@ -0,0 +1,10 @@
+// curentTarget - always refers to the element to which the event handler has been attached to
+// target - identifies the element on which the event occured
+
+const btns = document.querySelectorAll('.btn');
+btns.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    console.log(e.currentTarget);
+    e.currentTarget.style.color = 'green';
+  });
+});
