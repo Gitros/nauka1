f4ed95a6d52daa39db69eb2c60a3158de59b33ef
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-21 21:43:29 +0100

    event object

---
+++ app.js
@@ -0,0 +1,13 @@
+// event object argument e,evt
+// info about triggered event
+// event.type
+// event.currentTarget
+// this keyword
+// preventDefault() - prevents default behavior
+
+const heading = document.querySelector('h1');
+const btn = document.querySelector('.btn');
+
+heading.addEventListener('click', function (event) {
+  console.log(event.currentTarget);
+});
