f62939e930157ab6846258afd22994a0b14bbb37
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-21 21:45:10 +0100

    pt2

--- app.js
+++ app.js
@@ -11,3 +11,7 @@ const btn = document.querySelector('.btn');
 heading.addEventListener('click', function (event) {
   console.log(event.currentTarget);
 });
+btn.addEventListener('click', function (event) {
+  event.currentTarget.classList.add('blue');
+  console.log(event.type);
+});
