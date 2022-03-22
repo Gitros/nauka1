e602415cb50412b6b06e4c23c6ff940105b3fed3
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-22 19:12:55 +0100

    the end of the course.

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ localStorage.setItem('friends', JSON.stringify(friends));
 
 const values = JSON.parse(localStorage.getItem('friends'));
 console.log(values[0]);
+
+let fruits;
+if (localStorage.getItem('fruits')) {
+  fruits = JSON.parse(localStorage.getItem('fruits'));
+} else {
+  fruits = [];
+}
+// fruits.push('apple');
+fruits.push('orange');
+localStorage.setItem('fruits', JSON.stringify(fruits));
+console.log(fruits);
