93abab5b75318a65413953dae267d8ed361ca6d4
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-22 16:29:50 +0100

    event bubbling capturing

---
+++ app.js
@@ -0,0 +1,20 @@
+// allows select dynamic elements
+// event propogation - order the events are fired
+// event bubbling - clicked element first then bubbles up -- default
+// event capturing - fires at the root and fires until reaches target
+
+const container = document.querySelector('.container');
+const list = document.querySelector('.list-items');
+
+function showBubbling(e) {
+  console.log('current target', e.currentTarget);
+  console.log('target', e.target);
+  if (e.target.classList.contains('link')) {
+    console.log('you clicked on the link');
+  }
+}
+
+list.addEventListener('click', showBubbling);
+// container.addEventListener('click', showBubbling);
+// document.addEventListener('click', showBubbling);
+// window.addEventListener('click', showBubbling);
