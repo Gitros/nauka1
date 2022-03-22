11c60f14d6e9f2f268a994187097cf1461220630
Gitros <31899790+Gitros@users.noreply.github.com>
2022-03-22 18:54:19 +0100

    local storage pt1

---
+++ app.js
@@ -0,0 +1,6 @@
+// Web Storage API - provided by browser
+// session storage, local storage
+// setItem, getItem, removeItem, clear
+
+localStorage.setItem('name', 'john');
+sessionStorage.setItem('name', 'john');
